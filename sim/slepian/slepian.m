41f19fa2acd2f769cb239288e373a884dc2aa532
Andréas Sundström <andsunds@student.chalmers.se>
2017-08-17 21:20:48 +0200

    I have discovered that having a symmetric kernel-matrix improves computration times by a factor of 5 - 10 times! It is thus not a good idea to use Simpson's method if the kernel was symmetric fom the begining.

--- sim/slepian/slepian.m
+++ sim/slepian/slepian.m
@@ -12,7 +12,7 @@
 clc;clear all; clf
 
 
-L=5;                              %prec of int approx
+L=200;                               %prec of int approx
 T=1;                                %timespan
 x=linspace(-T/2,T/2,L)';            %the free time variable of kernel
 y=linspace(-T/2,T/2,L)';            %integrated variable in the kernel
@@ -22,15 +22,43 @@ W=8;                    %freq
 Ks=sin(W*xy)./xy/pi;    %kernel
 Ks(isnan(Ks))=W/pi;     %sin(W*(x-y))/(pi*(x-y)) -> W/pi when (x-y)->0
 
-
+tic
 [V,D]=eig(Ks);  %eigenfunctions and eigenvalues
-D=diag(D)/L;    %vector form, and normalizing to the wanted eigenvalues
+D=diag(D)*T/L;    %vector form, and normalizing to the wanted eigenvalues
+toc
 
 i=find(D>1e-4);                 %finds the largest eigenvlaues
-fprintf('D = %1.4d,\t\n',D(i)); %displays those eigenvalues
-plot(V(:,i));                   %plots correspoding eigenfunctions
+fprintf('D = %1.4d,\t\n',sort(D(i), 'descend')); %displays those eigenvalues
+plot(x,V(:,i));                   %plots correspoding eigenfunctions
 
 
+%% D=1, Simpson's method
+clc;clear all; clf
+% ¡Significantly slower (factor 10), due to Kmat no longer being symmetric!
+
+L=2*4;                              %prec of int approx
+T=1;                                %timespan
+x=linspace(-T/2,T/2,L)';            %the free time variable of kernel
+y=linspace(-T/2,T/2,L)';            %integrated variable in the kernel
+xy=(repmat(x,1,L)-repmat(y',L,1));  %combine x and y into a matrix of x.-y
+
+W=8;                    %freq
+Ks=sin(W*xy)./xy/pi;    %kernel
+Ks(isnan(Ks))=W/pi;     %sin(W*(x-y))/(pi*(x-y)) -> W/pi when (x-y)->0
+
+tic
+U=2*(1+(-1).^(1:L)) + (1-(-1).^(1:L));
+U(1)=1; U(L)=1;
+
+Kmat= T/(3*L) * repmat(U,L,1).*Ks;
+
+[V,D]=eig(Kmat);  %eigenfunctions and eigenvalues
+D=diag(D);    %vector form, and normalizing to the wanted eigenvalues
+toc
+
+i=find(D>1e-4);                 %finds the largest eigenvlaues
+fprintf('D = %1.4d,\t\n',sort(D(i), 'descend')); %displays those eigenvalues
+plot(x,V(:,i));                   %plots correspoding eigenfunctions
 
 
 
@@ -38,30 +66,57 @@ plot(V(:,i));                   %plots correspoding eigenfunctions
 clc;clear all; clf
 
 
-L=500;                  %prec of int approx
+L=1000;                  %prec of int approx
 eps=.1/L;               %can't have y=0
 x=linspace(eps,1,L)';   %the free time variable of kernel
 y=linspace(eps,1,L)';   %integrated freq variable in the kernel
 [Y,X]=meshgrid(x,y);    %combine x and y into mesh (note order: [Y,X])
 
-c=1;                        % c = \Omega T
+c=8;                        % c = \Omega T
 N=0;
 Ks=besselj(N,c*X.*Y).*Y;    %kernel
 %Ks(isnan(Ks))=W/pi;        %sin(W*(x-y))/(pi*(x-y)) -> W/pi when (x-y)->0
 %Ks
-
+tic
 [V,D]=eig(Ks);  %eigenfunctions and eigenvalues
 D=diag(D)/L;    %vector form, and normalizing
-
+toc
 
 d=2;                            %dimensions
 DD=(c/2/pi)^d.*abs(2*pi*D).^2;  %have to change normalization to get the wanted eigenvalues
 i=find(DD>1e-4);                %finds the largest eigenvlaues
 
 fprintf('DD = %1.4d,\t\n',sort(DD(i),1,'descend')); %displays those eigenvalues
-plot(V(:,i));                                       %plots correspoding eigenfunctions
+plot(x,V(:,i));                                       %plots correspoding eigenfunctions
 
 
+
+%% D=2, p=0, even kernel
+clc;clear all; clf
+%Once again, modifying to get an EVEN kernel saves around a factor 10 (!)
+
+L=1000;                  %prec of int approx
+eps=.1/L;               %can't have y=0
+x=linspace(eps,1,L)';   %the free time variable of kernel
+y=linspace(eps,1,L)';   %integrated freq variable in the kernel
+[Y,X]=meshgrid(x,y);    %combine x and y into mesh (note order: [Y,X])
+
+c=8;                        % c = \Omega T
+N=0;
+Ks=besselj(N,c*X.*Y).*sqrt(c*Y.*X);    %kernel
+%Ks(isnan(Ks))=W/pi;        %sin(W*(x-y))/(pi*(x-y)) -> W/pi when (x-y)->0
+%Ks
+tic
+[V,D]=eig(Ks);  %eigenfunctions and eigenvalues
+D=diag(D)/L;    %vector form, and normalizing
+toc
+
+d=2;                            %dimensions
+DD=(c/2/pi)^d.*abs(2*pi*D).^2/c;  %have to change normalization to get the wanted eigenvalues
+i=find(DD>1e-4);                %finds the largest eigenvlaues
+
+fprintf('DD = %1.4d,\t\n',sort(DD(i),1,'descend')); %displays those eigenvalues
+plot(x, V(:,i)./repmat(sqrt(x),1,length(i)));                                       %plots correspoding eigenfunctions
 
 
 
