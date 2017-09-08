1852f6d9a89a8585da242c4518d85d2c1f74966e
Andréas Sundström <andsunds@student.chalmers.se>
2017-09-08 09:46:36 +0200

    This is what was used to plot the figure in the final report

--- sim/slepian/data/figures.m
+++ sim/slepian/data/figures.m
@@ -26,20 +26,31 @@ clc;clf;clear all
 
 load('data/D2_ring-bessel-0_L-1000_2017-09-02.mat')
 
-plot(C/2/pi,data(:,1),'-k'), hold on
-plot(C/2/pi,data(:,2:end))
+stylecell={'k-', 'b-', 'r-', 'g-', 'm-'};
 
-
+plot(C/2/pi,data(:,1),stylecell{1}), hold on
+for j=1:3
+plot(C/2/pi,data(:,2*j),stylecell{j+1})
+%plot(C/2/pi,data(:,2:end))
+end
+%
 xtext='$R_\mathrm{T}/\lambda_{\mathrm{min}}$';
 xlabel(xtext, 'interpreter','latex')
 ytext='Energy fraction';%'$||E||_\mathrm{T}^2/||E||_{\infty}^2$';
 ylabel(ytext, 'interpreter','latex')
 set(gca, 'fontsize',18, 'xlim',[0,2])
 
-lcell=cellstr(num2str(Q','$q=%0.2f$'));
+f=@(c) (besselj(0,c).^2-besselj(-1,c).*besselj(1,c));
+HS=10;
+C=linspace(0,4*pi,1000);
+plot(C/2/pi, f(C)./f(HS*C)/HS^2,'--m')
+
+
+
+lcell=cellstr(num2str([Q([1,2,4,6]),1]','$q=%0.1f$'));
 l=legend(lcell);
-%set(l, 'location','NorthWest','interpreter','latex')
-set(l, 'position',[0.195,.74,0,0],'interpreter','latex')
+set(l, 'location','NorthWest','interpreter','latex')
+%set(l, 'position',[0.195,.74,0,0],'interpreter','latex')
 
 load('data/D2_ring-bessel-AB-0_L-1000_2017-09-02.mat');
 plot(C/2/pi,data,'k--')
@@ -47,8 +58,13 @@ hold on
 
 %load('data/D2_ring-0_L-2000_2017-08-19.mat');
 load('data/D2_ring-bessel-AB-0_L-1000_2017-09-01.mat');
-plot(0,0,0,0)
-plot(C/2/pi,data,'--')
+
+stylecell={'k--', 'b--', 'r--', 'g--', 'm--'};
+for j=1:3
+plot(C/2/pi,data(:,2*j-1),stylecell{j+1})
+%plot(C/2/pi,data(:,2:end))
+end
+
 
 
 
